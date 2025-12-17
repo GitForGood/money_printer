ef9e6c7d4ce2a2184c76dd73cb39872a74f0c509
oliver <oliversofta@gmail.com>
2025-12-17 16:03:34 +0100

    import of old project

---
+++ app/app.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="flex justify-center h-screen">
+    <!--
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+    -->
+
+    <bracketed-button @click="openBuyMenu">BUY</bracketed-button>
+    <bracketed-button>SELL</bracketed-button>
+    <bracketed-button @click="watch">{{ watched ? 'HIDE' : 'SHOW'}}</bracketed-button>
+    
+    <sidebar />
+    <bracketed-button>SELL</bracketed-button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const watched = ref(false)
+  const { open } = useSidebar()
+
+  function watch() {
+    watched.value = !watched.value
+  }
+
+  function openBuyMenu() {
+    open(null, 'Buy Money')
+  }
+</script>
---
+++ app/components/bracketed-button.vue
@@ -0,0 +1,55 @@
+<template>
+  <button :disabled="props.disabled" @click="handleClick">
+    <span>[</span>
+    <slot></slot>
+    <span>]</span>
+  </button>
+</template>
+
+<script lang="ts" setup>
+
+const props = defineProps({
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+})
+
+const emit = defineEmits([
+  'click',
+])
+
+function handleClick() {
+  emit('click')
+}
+
+</script>
+
+<style scoped>
+button {
+  font-size: 16px;
+  font-family:'Courier New', Courier, monospace;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0px;
+  padding: 4px 12px;
+  border: none;
+  cursor: pointer;
+  transition: gap 0.2s ease-in-out, padding 0.2s ease-in-out;
+}
+
+button:hover, button:focus-visible {
+  gap: 8px;
+  padding: 4px 4px;
+}
+
+button:active {
+  gap: 4px;
+  padding: 4px 8px;
+}
+
+button:disabled {
+  cursor: not-allowed;
+}
+</style>
